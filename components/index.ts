30a6e9d3dadf7aa57d12f9f2f34bacd26c76d806
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-10-29 20:12:03 +0530

    commit

--- components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AnimatedLoginForm } from './AnimatedLoginForm';
-export { default as AnimatedSignUpForm } from './AnimatedSignUpForm';
-export { default as RiveBear } from './RiveBear';
