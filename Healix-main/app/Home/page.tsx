c7cfb43bdddd1d9f7c570deca22642b61068e1a3
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-11-03 22:49:44 +0530

    commit

--- Healix-main/app/Home/page.tsx
+++ Healix-main/app/Home/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <main className="px-4 md:px-8 lg:px-10 flex flex-col md:flex-row justify-around items-center gap-6 md:gap-4 pt-0 pb-4 md:py-0">
             <Toaster />
             {}
-            <div className="w-full md:hidden flex justify-center items-center order-1 my-auto">
+            <div className="w-full md:hidden flex justify-center items-center order-1 my-auto mt-8 sm:mt-12">
                 <Image
                     src="/neurology.png"
                     alt="Healix Mental Health"
